90a61264a56fbda1913ca28f268bd4ac83a56faa
uhanuu <hwryu990927@gmail.com>
2024-03-05 21:50:10 +0900

    refactor: 목적지 목록 수정

--- src/main/java/com/weshare/api/v1/domain/schedule/Destination.java
+++ src/main/java/com/weshare/api/v1/domain/schedule/Destination.java
@@ -4,9 +4,18 @@ import java.util.Arrays;
 
 public enum Destination {
     SEOUL("서울"),
+    GYEONGGI("경기"),
     BUSAN("부산"),
-    Gangneung("강릉");
-//
+    GANGNEUNG("강릉"),
+    INCHEON("인천"),
+    SUWON("수원"),
+    DAEGU("대구"),
+    DAEJEON("대전"),
+    JEJU("제주"),
+    GWANGJU("광주"),
+    ULSAN("울산"),
+    JEONJU("전주"),
+    CHUNCHEON("춘천");
     private final String name;
 
     Destination(String name) {
