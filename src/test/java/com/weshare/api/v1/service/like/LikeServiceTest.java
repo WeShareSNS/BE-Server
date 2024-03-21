e37d5a9df7cd54a6fc16d2f89a05d86d64daf4c5
uhanuu <hwryu990927@gmail.com>
2024-03-21 17:49:30 +0900

    test: 좋아요 기능

--- src/test/java/com/weshare/api/v1/service/like/LikeServiceTest.java
+++ src/test/java/com/weshare/api/v1/service/like/LikeServiceTest.java
@@ -1,12 +1,25 @@
 package com.weshare.api.v1.service.like;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.weshare.api.v1.controller.like.dto.CreateLikeDto;
+import com.weshare.api.v1.controller.like.dto.DeleteLikeDto;
 import com.weshare.api.v1.domain.schedule.Destination;
 import com.weshare.api.v1.domain.schedule.Schedule;
+import com.weshare.api.v1.domain.schedule.exception.ScheduleNotFoundException;
 import com.weshare.api.v1.domain.user.User;
 import com.weshare.api.v1.repository.schedule.ScheduleTestSupport;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LikeServiceTest extends ScheduleTestSupport {
 
@@ -15,15 +28,59 @@ class LikeServiceTest extends ScheduleTestSupport {
 
     @Test
     @Transactional
+    public void 특정_게시물에_좋아요_등록을_확인할_수_있다() {
+        // given
+        User user = createUserAndSave("like@test.com", "like1", "like");
+        Schedule schedule = createAndSaveSchedule("title", Destination.DAEGU, user);
+        CreateLikeDto createLikeDto = new CreateLikeDto(schedule.getId(), user);
+        // when
+        CreateLikeResponse createLikeResponse = likeService.saveScheduleLike(createLikeDto);
+        // then
+        assertThat(user.getName()).isEqualTo(createLikeResponse.likerName());
+    }
+
+    @Test
+    @Transactional
+    public void 존재하지_않는_게시물에_좋아요_등록시_예외가_발생한다() {
+        // given
+        User user = createUserAndSave("like@test.com", "like1", "like");
+        CreateLikeDto createLikeDto = new CreateLikeDto(0L, user);
+        // when // then
+        assertThatThrownBy(() -> likeService.saveScheduleLike(createLikeDto))
+                .isInstanceOf(ScheduleNotFoundException.class);
+    }
+
+    @Test
+    @Transactional
+    public void 사용자는_특정_게시물에_등록한_좋아요를_삭제할_수_있다() {
+        // given
+        User user = createUserAndSave("like@test.com", "like1", "like");
+        Schedule schedule = createAndSaveSchedule("title", Destination.DAEGU, user);
+        CreateLikeDto createLikeDto = new CreateLikeDto(schedule.getId(), user);
+        CreateLikeResponse createLikeResponse = likeService.saveScheduleLike(createLikeDto);
+        // when
+        DeleteLikeDto deleteLikeDto = new DeleteLikeDto(schedule.getId(), createLikeResponse.likeId(), user);
+        likeService.deleteScheduleLike(deleteLikeDto);
+        // then
+        List<FindAllScheduleLikeDto> allScheduleLike = likeService.findAllScheduleLike(schedule.getId());
+        assertThat(allScheduleLike).hasSize(0);
+    }
+
+    @Test
+    @Transactional
     public void 특정_게시물에_좋아요_정보를_확인할_수_있다() {
         // given
         User user = createUserAndSave("like@test.com", "like1", "like");
         Schedule schedule = createAndSaveSchedule("title", Destination.DAEGU, user);
-
+        CreateLikeDto createLikeDto = new CreateLikeDto(schedule.getId(), user);
+        CreateLikeResponse createLikeResponse = likeService.saveScheduleLike(createLikeDto);
         // when
-        likeService.findAllScheduleLike(schedule.getId());
-
+        List<FindAllScheduleLikeDto> allScheduleLike = likeService.findAllScheduleLike(schedule.getId());
         // then
+        assertThat(allScheduleLike).hasSize(1)
+                .extracting("likeId", "likerName", "likedTime")
+                .containsExactly(
+                        Tuple.tuple(createLikeResponse.likeId(), user.getName(), createLikeResponse.likedTime())
+                );
     }
-
 }
