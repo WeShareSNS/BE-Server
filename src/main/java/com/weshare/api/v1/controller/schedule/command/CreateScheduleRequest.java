88e4555ee60a2fb55f8df519d81915162286fd30
uhanuu <hwryu990927@gmail.com>
2024-03-23 16:39:18 +0900

    style: 주석 제거

--- src/main/java/com/weshare/api/v1/controller/schedule/command/CreateScheduleRequest.java
+++ src/main/java/com/weshare/api/v1/controller/schedule/command/CreateScheduleRequest.java
@@ -9,7 +9,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
--- src/main/java/com/weshare/api/v1/token/RefreshTokenRepository.java
+++ src/main/java/com/weshare/api/v1/token/RefreshTokenRepository.java
@@ -1,12 +1,6 @@
 package com.weshare.api.v1.token;
 
-import com.weshare.api.v1.domain.user.User;
-import com.weshare.api.v1.token.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long>, RefreshTokenCustomRepository {
 }
